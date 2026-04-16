3872955a420c2e060b19865ab3025db8a3dbeecb
hossain15-5299-creator <hossain15-5299@diu.edu.bd>
2026-04-16 23:41:18 +0600

    Code updated 4

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import logo from '../../assets/logo.png';
 import { NavLink } from 'react-router';
 import { Home, Clock, BarChart3 } from 'lucide-react';
 
 const Navbar = () => {
     const activeClass = ({ isActive }) => 
-        isActive ? "flex items-center gap-1 text-green-700 font-bold border-b-2 border-green-700" 
-                 : "flex items-center gap-1 text-gray-600 hover:text-green-700";
+        isActive ? "flex items-center gap-1 text-[#1A4D2E] font-bold border-b-2 border-[#1A4D2E]" 
+                 : "flex items-center gap-1 text-gray-600 hover:text-[#1A4D2E]";
 
     return (
         <nav className="bg-white border-b sticky top-0 z-50">
             <div className='flex justify-between items-center py-3 container mx-auto px-4'>
-                <div className="flex items-center gap-2">
-                   
+                <div className="flex items-center text-2xl font-[900] tracking-tighter leading-none">
                     <span className="text-[#1e293b]">Keen</span>
-                    
                     <span className="text-[#1A4D2E]">Keeper</span>
                 </div>
 
